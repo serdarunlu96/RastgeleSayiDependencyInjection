116471ccb28c33575683e5f4054224d4a1ca9615
serdarunlu96 <71671958+serdarunlu96@users.noreply.github.com>
2023-08-22 11:24:27 +0300

    Add project files.

---
+++ RastgeleSayiDependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RastgeleSayiDependencyInjection.Models;
+using RastgeleSayiDependencyInjection.Services;
+using System.Diagnostics;
+
+namespace RastgeleSayiDependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RastgeleBirSayi _rastgeleBirSayi;
+
+        public HomeController(ILogger<HomeController> logger, RastgeleBirSayi rastgeleBirSayi)
+        {
+            _logger = logger;
+            _rastgeleBirSayi = rastgeleBirSayi;
+        }
+
+        public IActionResult Index([FromServices] RastgeleBirSayi rastgeleBirSayi)
+        {
+            RastgeleBirSayi rbs = new RastgeleBirSayi();  // MANUEL
+            ViewBag.RastgeleSayi = rbs.Deger;
+
+            ViewBag.RastgeleSayiHomeEnjekte = _rastgeleBirSayi.Deger;  // DI
+             
+            ViewBag.RastgeleSayiActionEnjekte = rastgeleBirSayi.Deger; // Controller Enj.
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RastgeleSayiDependencyInjection/Services/RastgeleBirSayi.cs
@@ -0,0 +1,7 @@
+﻿namespace RastgeleSayiDependencyInjection.Services
+{
+    public class RastgeleBirSayi
+    {
+        public int Deger { get; } = new Random().Next(1, 101);
+    }
+}
